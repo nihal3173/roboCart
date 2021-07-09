74492750dcf352cfa7171c1c476a0ed17e6e5955
nihal3173 <85364180+nihal3173@users.noreply.github.com>
2021-07-09 16:09:20 -0400

    updated_demo_working

--- MainActivity.java
+++ MainActivity.java
@@ -1,6 +1,9 @@
 package com.reeman.demo3128;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 
@@ -30,44 +33,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initView() {
-        Button btn1 = findViewById(R.id.btn1);
-        Button btn2 = findViewById(R.id.btn2);
-        Button btn3 = findViewById(R.id.btn3);
-        Button btn4 = findViewById(R.id.btn4);
         Button btn5 = findViewById(R.id.btn5);
         Button btn6 = findViewById(R.id.btn6);
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
-        btn4.setOnClickListener(this);
+        Button btn7 = findViewById(R.id.btn7);
+        Button btn8 = findViewById(R.id.btn8);
         btn5.setOnClickListener(this);
         btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btn1:
-                RobotActionProvider.getInstance().moveFront(20);
-                break;
-            case R.id.btn2:
-                RobotActionProvider.getInstance().moveBack(20, 0);
-                break;
-            case R.id.btn3:
-                RobotActionProvider.getInstance().moveLeft(90, 0);
-                break;
-            case R.id.btn4:
-                RobotActionProvider.getInstance().moveLeft(-90, 0);
-                break;
             case R.id.btn5:
                 //navigate to point "x1", you should mark x1 in your map in advance.
                 RobotActionProvider.getInstance().sendRosCom("point[x1]");
+                new Handler().postDelayed(() -> RobotActionProvider.getInstance().sendRosCom("point[x2]"), 30000 );//time in millisecond
                 break;
+
             case R.id.btn6:
-                //navigate to charge pile, you should mark charge pile in your map in advance.
-                RobotActionProvider.getInstance().sendRosCom("point_charge[charge_pile]");
+                //navigate to charge pile, mark charge pile in map, direct docking.
+                RobotActionProvider.getInstance().sendRosCom("point_charge[charging_pile]");
+                RobotActionProvider.getInstance().sendRosCom("goal: just_charge");
+                //RobotActionProvider.getInstance().sendRosCom("bat: uncharge");
+                break;
+
+            case R.id.btn7:
+               // RobotActionProvider.getInstance().sendRosCom("point[x3]");
+                break;
+
+            case R.id.btn8:
+                //RobotActionProvider.getInstance().shutDown();
+                break;
+
             default:
-                //for more command, please refer to our document
+
                 break;
         }
     }
